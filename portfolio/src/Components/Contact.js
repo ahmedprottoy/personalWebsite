59862ceb73b03026d96b6c4e7fe68ceca6289699
Prottoy <prottoy.r.ahmed@gmail.com>
2022-11-07 02:59:00 +0600

    done

---
+++ portfolio/src/Components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div className="p-10 bg-gray-300">
+      <div className="flex flex-col items-center justify-center">
+        <p className="text-4xl font-semibold ">Get In Touch</p>
+        <span className="bg-sky-500 w-20 h-2 my-10 rounded-2xl "></span>
+        <div>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
